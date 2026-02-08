7fbea41df881cdcc99cb865d8d42342c598f6b03
Hailemedhn Tegenge <hailemedhntegegne@gmail.com>
2026-02-08 01:58:36 +0100

    Longest Substring without Repeating even faster

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -2,17 +2,17 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         lengthOfLongestStringWithoutRepeating = 0
         longestStringWithoutRepeating = ""
-        i = 0
+        i,jumpIndex = 0,0
         while i < len(s):
             if s[i] not in longestStringWithoutRepeating:
                 longestStringWithoutRepeating += s[i]
             else:
                 if len(longestStringWithoutRepeating) > lengthOfLongestStringWithoutRepeating:
                     lengthOfLongestStringWithoutRepeating = len(longestStringWithoutRepeating)
-                if len(longestStringWithoutRepeating) == 1:
-                    pass
-                else:
-                    longestStringWithoutRepeating = longestStringWithoutRepeating[1:]
-                    i -= 1
-            i += 1
+                
+                jumpIndex = longestStringWithoutRepeating.index(s[i]) + 1
+                longestStringWithoutRepeating = longestStringWithoutRepeating[jumpIndex:]
+                i -= 1
+                jumpIndex = 0
+            i +=  1
         return max(lengthOfLongestStringWithoutRepeating, len(longestStringWithoutRepeating))
